135665cc4622085deedfb9852749dfdaa8a39464
岸谷叙南 <JonahKishiya@kishitani-no-MacBook-Pro.local>
2018-01-31 19:00:46 +0900

    routingの設定、アクションの書き込み

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,4 +2,7 @@ class MessagesController < ApplicationController
   def index
     @messages = "Hello World!"
   end
+
+  def create
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
- root 'messages#index'
- resource :user, only: [:edit, :update]
+  root 'groups#index'
+  resource :user, only: [:edit, :update]
+  resource :groups, only: [:new, :create, :edit, :update] do
+    resource :messages, only: [:index, :create]
+  end
 
 
 end
