39539c6837551ebcf4031ebd73f83124af6a54e3
岸谷叙南 <JonahKishiya@kishitani-no-MacBook-Pro.local>
2018-02-05 17:49:38 +0900

    アソシエーションの編集 group_usersのテーブルのカラム名の編集 グループ作成画面の編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :group_users
+  has_many :group_users, foreign_key: 'user_id'
   has_many :groups, through: :group_users
 end
