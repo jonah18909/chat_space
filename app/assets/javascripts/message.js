569c59fccd608c30b24db69150ede394dbe259e8
岸谷叙南 <JonahKishiya@kishitani-no-MacBook-Pro.local>
2018-03-07 17:47:19 +0900

    メッセージ送信機能の非同期通信のためのjqueryの記述 jsonの実装 javascriptファイルの作成

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,48 @@
+$(function(){
+  function buildHTML(message){
+    var chatImage = "";
+    if (message.image){
+      chatImage = `<img class='chat-main__message-image', src='${message.image}'> `}
+    var html = `
+                  <div class="chat-main__message clearfix">
+                    <div class="chat-main__message-name">
+                    ${message.name}
+                    </div>
+                  <div class="chat-main__message-time">
+                  ${message.time}
+                  </div>
+                  <div class="chat-main__message-body">
+                    <p class="chat-main__message-text">
+                    ${message.text}
+                    </p>
+                    ${chatImage}
+                  </div>
+                `
+    return html;
+  }
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    console.log(this)
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      console.log('成功')
+      $('.chat-main__body--messages-list').append(html);
+      $('#message_text').val('');
+      $('.chat-main__body').animate({scrollTop:$('document').height }, 1500);
+    })
+    .fail(function(){
+      alert('error');
+    });
+  });
+});
